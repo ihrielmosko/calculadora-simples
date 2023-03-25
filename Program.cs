09b2066544aaecf4fb353d6f9d10f1a2a0ae5690
ihriel.mosko <ihriel.mosko@gmail.com>
2023-03-25 03:30:09 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace teste
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int teste = 4;
+
+            if (teste == 1) {
+                Console.WriteLine("igual a 1");
+            } else {
+                for (int i = 0; i < teste; i++) {
+                    Console.WriteLine(i);
+                }
+            }
+
+            
+        }
+    }
+}
