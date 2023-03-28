376e504b79cc04928ae3f143d30aefbd3ee65d52
ihriel.mosko <ihriel.mosko@gmail.com>
2023-03-28 16:03:47 -0300

    primeira versão estável

--- Program.cs
+++ Program.cs
@@ -2,21 +2,45 @@
 
 namespace teste
 {
-    class program
+    class Program
     {
         static void Main(string[] args)
         {
-            int teste = 4;
+            Console.WriteLine("");
+            Console.WriteLine("Escolha o operador (digite o número designado)");
+            Console.WriteLine("0 - soma");
+            Console.WriteLine("1 - subtração");
+            Console.WriteLine("2 - multiplicação");
+            Console.WriteLine("3 - divisão");
+            string stringOperador = Console.ReadLine();
+            Console.WriteLine("");
 
-            if (teste == 1) {
-                Console.WriteLine("igual a 1");
-            } else {
-                for (int i = 0; i < teste; i++) {
-                    Console.WriteLine(i);
-                }
-            }
+            Console.WriteLine("Digite o primeiro número");
+            string stringA = Console.ReadLine();
+            Console.WriteLine("");
 
-            
+            Console.WriteLine("Digite o segundo número");
+            string stringB = Console.ReadLine();
+            Console.WriteLine("");
+
+            //Conversão das informações de texto em int
+
+            int a = Convert.ToInt32(stringA);
+            int b = Convert.ToInt32(stringB);
+            int operador = Convert.ToInt32(stringOperador);
+
+            //cálculo
+
+            Console.WriteLine("Resultado");
+            int resultado = 0;
+
+            if (operador == 0) {resultado = a + b;}
+            else if (operador == 1) {resultado = a - b;}
+            else if (operador == 2) {resultado = a * b;}
+            else if (operador == 3) {resultado = a / b;}
+            else {Console.WriteLine("Operador inválido");}
+            Console.WriteLine(resultado);
+            Console.WriteLine("");
         }
     }
 }
